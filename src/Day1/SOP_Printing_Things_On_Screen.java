222d22723abd2f8a72c490930e35f8f618a06dd2
Shashank <shashank.agrawal_cs18@gla.ac.in>
2021-07-18 00:25:59 +0530

    sop versions

--- src/Day1/SOP_Printing_Things_On_Screen.java
+++ src/Day1/SOP_Printing_Things_On_Screen.java
@@ -3,6 +3,15 @@ package Day1;
 public class SOP_Printing_Things_On_Screen {
 
     public static void main(String[] args) {
+
         System.out.println("Hello My Name is Shashank Agrawal");
+        System.out.print("Hello My Name is Shashank" + " ");  // no line change
+        System.out.println("Agrawal");
+
+        int a = 10;
+        System.out.printf("The Value of a :%d", a);
+        System.out.println();
+
+        System.out.format("%03d", a);
     }
 }
